e8f57627fab8bdd4e61f4f62f2fa71a395edeccc
Aluno <Aluno@DESKTOP-O31M9M7>
2026-09-24 10:24:18 -0300

    24/09/2026 Troquei as fontes

--- AppHamburguerFilme/MauiProgram.cs
+++ AppHamburguerFilme/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace AppHamburguerFilme
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Hiddencinema.ttf", "Hiddencinema");
                 });
 
 #if DEBUG
